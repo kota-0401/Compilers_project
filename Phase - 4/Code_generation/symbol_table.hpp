bb6ae07f053335f5c7cbac033c8c339516bd6653
VishalBurra27 <102515414+VishalBurra27@users.noreply.github.com>
2023-11-26 17:19:55 +0530

    Update symbol_table.hpp

--- Phase - 4/Code_generation/symbol_table.hpp
+++ Phase - 4/Code_generation/symbol_table.hpp
@@ -1,3 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+extern char* yytext;
+
+struct typerec {
+    string name;
+    int scope;
+    string type;
+    string eletype;
+    int no_of_dim;
+};
+
+struct param {
+    string name;
+    string eletype;
+};
+
+struct f_unique {
+    string name;
+    vector<param> param_list;
+    string return_type;
+    string return_eletype;
+    bool is_defined;
+};
+
+vector<f_unique> function_table;
+vector<vector<typerec>> local_symbol_table(1);
+
+vector<param> param_list;
+vector<string> call_arg_list;
+int scope = 0;
+int add_function_value = 0;
+int no_of_dim = 0;
+string eletype = "";
+string type = "simple";
+
+void create_symbol_table() {
+    vector<typerec> new_table;  // Create a new empty symbol table
+    local_symbol_table.push_back(new_table);  // Add the new symbol table to the vector
+    scope++;
+}
+
+void delete_symbol_table() {
+    local_symbol_table.pop_back();  // Remove the last symbol table
+    scope--;
+}
+
+string check_type_eletype(string name) {
+    for (int i = 0; i < function_table.size(); i++) {
+        if (function_table[i].name == name) {
+            if(function_table[i].param_list.size() == call_arg_list.size()) {
+                for (int j = 0; j < call_arg_list.size(); j++) {
+                    if (function_table[i].param_list[j].eletype != call_arg_list[j]) {
+                        break;
+                    }
+                }
+                return function_table[i].return_eletype;
+            }
+        }
+    }
+    return "not_found";
+}
+
 bool type_checking(string from_type, string to_type) {
     // Define a set of valid type conversions for each type
     unordered_set<string> valid_conversions;
